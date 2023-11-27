197c3a6ec8ccba51fe76648fe9de2c85d953fe35
Mohammad Malmir <m.malmir666@gmail.com>
2023-11-28 01:57:07 +0330

    work on menu

--- main.js
+++ main.js
@@ -2,19 +2,32 @@ const navItem=document.querySelector('#nav__items');
 const openNavBtn=document.querySelector('#open__nav-btn');
 const closeNavBtn=document.querySelector('#close__nav-btn');
 
-const openNav=()=>{
-    navItem.style.display='flex';
-    openNavBtn.style.display='none';
-    closeNavBtn.style.display='inline-block';
-}
-openNavBtn.addEventListener('click', openNav);
 
-const closeNav=()=>{
-    navItem.style.display='none';
-    openNavBtn.style.display='inline-block';
-    closeNavBtn.style.display='none';
-}
-closeNavBtn.addEventListener('click',closeNav);
+ 
+   
+        openNavBtn.addEventListener('click',()=>{
+            navItem.style.display='flex';
+            openNavBtn.style.display='none';
+            closeNavBtn.style.display='inline-block';
+            if ((screen.width>1024)) {
+                //openNavBtn.style.display ='';
+                closeNavBtn.style.display ='';
+            }
+        });
+
+
+        closeNavBtn.addEventListener('click',()=>{
+            navItem.style.display='none';
+            openNavBtn.style.display='inline-block';
+            closeNavBtn.style.display='none';
+            if ((screen.width>1024)) {
+                openNavBtn.style.display ='';
+                //closeNavBtn.style.display ='';
+            }
+        });
+    
+      
+
+
 //CLOSE NAV MENU 
 //Attach listener function on state changes
-
