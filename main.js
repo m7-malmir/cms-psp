9a965b6aab08b074243317046034b5d731b29709
Mohammad Malmir <m.malmir666@gmail.com>
2023-11-28 23:03:33 +0330

    some file edit

--- main.js
+++ main.js
@@ -3,30 +3,31 @@ const openNavBtn=document.querySelector('#open__nav-btn');
 const closeNavBtn=document.querySelector('#close__nav-btn');
 
 
+    
+        if ((screen.width<1024)) {
+            openNavBtn.style.display ='inline-block';
+            closeNavBtn.style.display =''; 
+        }
+        // else{
+
  
    
-        openNavBtn.addEventListener('click',()=>{
-            navItem.style.display='flex';
-            openNavBtn.style.display='none';
-            closeNavBtn.style.display='inline-block';
-            if ((screen.width>1024)) {
-                //openNavBtn.style.display ='';
-                closeNavBtn.style.display ='';
-            }
-        });
-
-
-        closeNavBtn.addEventListener('click',()=>{
-            navItem.style.display='none';
-            openNavBtn.style.display='inline-block';
-            closeNavBtn.style.display='none';
-            if ((screen.width>1024)) {
-                openNavBtn.style.display ='';
-                //closeNavBtn.style.display ='';
-            }
-        });
+        //     openNavBtn.addEventListener('click',()=>{
+        //         navItem.style.display='flex';
+        //         openNavBtn.style.display='none';
+        //         closeNavBtn.style.display='inline-block';
+                
+                
+        //     });
     
-      
+    
+        //     closeNavBtn.addEventListener('click',()=>{
+        //         navItem.style.display='none';
+        //         openNavBtn.style.display='inline-block';
+        //         closeNavBtn.style.display='none';
+               
+        //     });
+        // }
 
 
 //CLOSE NAV MENU 
