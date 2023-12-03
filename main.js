8b1ce7a4ae6459d6b040cec6810dda67f37d8a8e
Mohammad Malmir <m.malmir666@gmail.com>
2023-12-03 22:02:42 +0330

    fix menu media query

--- main.js
+++ main.js
@@ -2,17 +2,12 @@ const navItem=document.querySelector('#nav__items');
 const openNavBtn=document.querySelector('#open__nav-btn');
 const closeNavBtn=document.querySelector('#close__nav-btn');
 
-
-    
-        // if ((screen.width<1024)) {
+ // if ((screen.width<1024)) {
         //     openNavBtn.style.display ='inline-block';
         //     closeNavBtn.style.display =''; 
         // }
         // else{
-
- 
-   
-            openNavBtn.addEventListener('click',()=>{
+   openNavBtn.addEventListener('click',()=>{
                 navItem.style.display='flex';
                 openNavBtn.style.display='none';
                 closeNavBtn.style.display='inline-block';
@@ -20,8 +15,7 @@ const closeNavBtn=document.querySelector('#close__nav-btn');
                 
             });
     
-    
-            closeNavBtn.addEventListener('click',()=>{
+ closeNavBtn.addEventListener('click',()=>{
                 navItem.style.display='none';
                 openNavBtn.style.display='inline-block';
                 closeNavBtn.style.display='none';
